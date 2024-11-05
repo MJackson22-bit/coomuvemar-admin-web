f63f991ccf18cf5cb09937d0dadc1697d8bccc11
MJackson22-bit <jm5444859@gmail.com>
2024-11-05 13:18:16 -0600

    feature: add register page

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -1,17 +1,8 @@
 <?php
 
 namespace App\Filament\Pages\Auth;
-
-use App\Models\BaseURL;
-use App\Models\GeneralData;
-use App\Models\User;
-use Exception;
 use Filament\Actions\Action;
-use Filament\Pages\Page;
-
 use Filament\Pages\Auth\Register as BaseRegister;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Http;
 
 class Register extends BaseRegister
 {
@@ -20,24 +11,6 @@ class Register extends BaseRegister
     {
         return parent::getRegisterFormAction()
             ->label('Registrarse');
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function handleRegistration(array $data): Model
-    {
-        $url = BaseURL::$BASE_URL . 'auth/register';
-        $response = Http::post(
-            url: $url,
-            data: $data
-        )->json();
-        if ($response['status'] === false) {
-            throw new Exception("Failed to create user: " . $response['message']);
-        }
-        return new User(
-            attributes: $response['data']
-        );
     }
 
     protected function getForms(): array
